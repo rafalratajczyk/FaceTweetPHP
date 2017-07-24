20e0e16d77d8a24fc9a6eff6da4c72e687d36e53
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-24 18:51:25 +0200

    add alert signed up

--- tweet/app/Http/routes.php
+++ tweet/app/Http/routes.php
@@ -4,3 +4,7 @@ Route::get('/', [
     'uses' => '\Tweet\Http\Controllers\HomeController@index',
     'as' => 'welcome'
 ]);
+
+Route::get('/alert', function () {
+    return redirect()->route('welcome')->with('info', 'Signed up!');
+});
