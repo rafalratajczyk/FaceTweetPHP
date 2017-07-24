7aa90080266ee1fd37b1daf735a8be1334e04aa3
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-24 18:36:28 +0200

    create welcome, fefault templates, partials

---
+++ tweet/resources/views/templates/partials/navigation.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Tweete</a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Timeline</a></li>
+                <li><a href="#">Friends</a></li>
+            </ul>
+            <form class="navbar-form navbar-left" role="search" action="#">
+                <div class="form-group">
+                    <input type="text" name="query" class="form-control" placeholder="Find people">
+                </div>
+                <button type="submit" class="btn btn-default">Search</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Dayle</a></li>
+                <li><a href="#">Update profile</a></li>
+                <li><a href="#">Sign up</a></li>
+                <li><a href="#">Sign in</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ tweet/resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('templates.default')
+
+@section('content')
+    <h3>Welcome to Tweete</h3>
+    <p>Created by Rafał</p>
+@stop
