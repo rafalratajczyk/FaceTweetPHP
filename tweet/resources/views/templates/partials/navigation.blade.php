7f0418ae05f02a142f32f5ea56ad62ab2a993cc8
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-25 16:24:40 +0200

    validation and showing stuff only when you are login

--- tweet/resources/views/templates/partials/navigation.blade.php
+++ tweet/resources/views/templates/partials/navigation.blade.php
@@ -4,21 +4,27 @@
             <a class="navbar-brand" href="{{ route('welcome') }}">Tweete</a>
         </div>
         <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="#">Timeline</a></li>
-                <li><a href="#">Friends</a></li>
-            </ul>
-            <form class="navbar-form navbar-left" role="search" action="#">
-                <div class="form-group">
-                    <input type="text" name="query" class="form-control" placeholder="Find people">
-                </div>
-                <button type="submit" class="btn btn-default">Search</button>
-            </form>
+            @if(Auth::check())
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Timeline</a></li>
+                    <li><a href="#">Friends</a></li>
+                </ul>
+                <form class="navbar-form navbar-left" role="search" action="#">
+                    <div class="form-group">
+                        <input type="text" name="query" class="form-control" placeholder="Find people">
+                    </div>
+                    <button type="submit" class="btn btn-default">Search</button>
+                </form>
+            @endif
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Dayle</a></li>
-                <li><a href="#">Update profile</a></li>
-                <li><a href="{{ route('auth.signup') }}">Sign up</a></li>
-                <li><a href="#">Sign in</a></li>
+                @if(Auth::check())
+                    <li><a href="#">{{ Auth::user()->getNameOrUsername() }}</a></li>
+                    <li><a href="#">Update profile</a></li>
+                    <li><a href="#">Sign out</a></li>
+                @else
+                    <li><a href="{{ route('auth.signup') }}">Sign up</a></li>
+                    <li><a href="{{ route('auth.signin') }}">Sign in</a></li>
+                @endif
             </ul>
         </div>
     </div>
