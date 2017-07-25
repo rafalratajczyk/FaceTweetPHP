79011ae509911ea0e7aef05bd56d72b45f187406
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-25 16:27:28 +0200

    modifed path for user

--- tweet/config/auth.php
+++ tweet/config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => Tweet\User::class,
+            'model' => Tweet\Models\User::class,
         ],
 
         // 'users' => [
