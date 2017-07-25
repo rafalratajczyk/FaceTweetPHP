ae69c2ebe7d054f6d49b461cebc4a730fc52842f
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-25 16:28:45 +0200

    function sign in get and post:

--- tweet/app/Http/Controllers/AuthController.php
+++ tweet/app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace Tweet\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Tweet\Models\User;
 use Illuminate\Http\Request;
 
@@ -21,12 +22,31 @@ class AuthController extends Controller
         ]);
 
         User::create([
-           'email' => $request->input('email'),
-           'username' => $request->input('username'),
-           'password' => bcrypt($request->input('password')),
+            'email' => $request->input('email'),
+            'username' => $request->input('username'),
+            'password' => bcrypt($request->input('password')),
         ]);
 
         return redirect()->route('welcome')->with('info', 'Account created succesfully');
 
+    }
+
+    public function getSignIn()
+    {
+        return view('auth.signin');
+    }
+
+    public function postSignIn(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($request->only(['email', 'password']), $request->has('remember'))) {
+            return redirect()->back()->with('info', 'Wrong email or password');
+        }
+
+        return redirect()->route('welcome')->with('info', 'Signed in');
     }
 }
