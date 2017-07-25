a3ea7111cdf2b9f716118e9b0a2b6c682c87419d
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-25 14:32:13 +0200

    change destination for userphp and modified

---
+++ tweet/app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tweet\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+
+class User extends Model implements AuthenticatableContract
+{
+    use Authenticatable;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+        'first_name',
+        'last_name',
+        'location'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
