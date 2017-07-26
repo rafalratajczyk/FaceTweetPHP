d70fb8e22c00ca9ee14719090890c13dad47e9d9
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-26 21:16:07 +0200

    function editing

--- tweet/app/Http/Controllers/ProfileController.php
+++ tweet/app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace Tweet\Http\Controllers;
 
+use Auth;
 use Tweet\Models\User;
 use Illuminate\Http\Request;
 
@@ -16,5 +17,28 @@ class ProfileController extends Controller
         }
         return view('profile.index')
             ->with('user', $user);
+    }
+
+    public function getEdit()
+    {
+        return view('profile.edit');
+    }
+
+    public function postEdit(Request $request)
+    {
+        $this->validate($request, [
+            'first_name' => 'alpha|max:60',
+            'last_name' => 'alpha|max:60',
+            'location' => 'max:30'
+        ]);
+
+        Auth::user()->update([
+           'first_name' => $request->input('first_name'),
+           'last_name' => $request->input('last_name'),
+           'location' => $request->input('location'),
+        ]);
+        return redirect()
+            ->route('profile.edit')
+            ->with('info', 'Success');
     }
 }
