6c5b1b85ccdb7c022224388888c78042c787b29e
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-29 12:11:55 +0200

    validations and buttons for sending request to be a friend

--- tweet/resources/views/profile/index.blade.php
+++ tweet/resources/views/profile/index.blade.php
@@ -6,6 +6,15 @@
             @include('user.partials.userblock')
         </div>
         <div class="col-lg-4 col-lg-offset-3">
+            @if(Auth::user()->hasFriendRequestPending($user))
+                <p>Waiting for {{ $user->getNameOrUsername() }} to accept your request</p>
+            @elseif(Auth::user()->hasFriendRequestReceived($user))
+                <a href="#" class="btn btn-primary">Accept friend request</a>
+            @elseif(Auth::user()->isFriendsWith($user))
+                <p>You and {{ $user->getNameOrUsername() }} are friends.</p>
+            @else
+                <a href="#" class="btn btn-primary">Add as friend</a>
+            @endif
             <h4>{{ $user->getFirstNameOrUsername() }}'s friends.</h4>
             @if(!$user->friends()->count())
                 <p>{{ $user->getFirstNameOrUsername() }} has no friends at all.</p>
