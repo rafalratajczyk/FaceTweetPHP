e731e23c505e5a2d93fb0909beaec9f7dac71f40
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-29 13:24:49 +0200

    unable to add friends myself

--- tweet/resources/views/profile/index.blade.php
+++ tweet/resources/views/profile/index.blade.php
@@ -9,11 +9,13 @@
             @if(Auth::user()->hasFriendRequestPending($user))
                 <p>Waiting for {{ $user->getNameOrUsername() }} to accept your request</p>
             @elseif(Auth::user()->hasFriendRequestReceived($user))
-                <a href="#" class="btn btn-primary">Accept friend request</a>
+                <a href="{{ route('friends.accept', ['username' => $user->username]) }}" class="btn btn-primary">Accept
+                    friend request</a>
             @elseif(Auth::user()->isFriendsWith($user))
                 <p>You and {{ $user->getNameOrUsername() }} are friends.</p>
-            @else
-                <a href="{{ route('friend.add', ['username' => $user->username]) }}" class="btn btn-primary">Add as friend</a>
+            @elseif(Auth::user()->id !== $user->id)
+                <a href="{{ route('friends.add', ['username' => $user->username]) }}" class="btn btn-primary">Add as
+                    friend</a>
             @endif
             <h4>{{ $user->getFirstNameOrUsername() }}'s friends.</h4>
             @if(!$user->friends()->count())
