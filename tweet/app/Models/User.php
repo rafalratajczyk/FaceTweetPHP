45d441082e66c645ce3f1bc4fb3117dde0074e55
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-31 19:15:31 +0200

    function statuses

--- tweet/app/Models/User.php
+++ tweet/app/Models/User.php
@@ -54,6 +54,11 @@ class User extends Model implements AuthenticatableContract
         return "https://www.gravatar.com/avatar/{{ md5($this->email) }}?d=mm&s=40";
     }
 
+    public function statuses()
+    {
+        return $this->hasMany('Tweet\Models\Status', 'user_id');
+    }
+
     public function friendsOfMine()
     {
         return $this->belongsToMany('Tweet\Models\User', 'friends', 'user_id', 'friend_id');
