57a2d5250586d412a84d2ec19ea4ac0056915395
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-31 19:51:15 +0200

    form for tweeting

--- tweet/resources/views/timeline/index.blade.php
+++ tweet/resources/views/timeline/index.blade.php
@@ -19,8 +19,49 @@
     </div>
     <div class="row">
         <div class="col-lg-5">
-
+            @if(!$statuses->count())
+                <p>Nothing to show in your timeline.</p>
+            @else
+                @foreach($statuses as $status)
+                    <div class="media">
+                        <a class="pull-left" href="{{ route('profile.index', ['username' => $status->user->username]) }}">
+                            <img class="media-object" alt="{{ $status->user->getNameOrUsername() }}" src="{{ $status->user->getAvatarUrl() }}">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading"><a href="{{ route('profile.index', ['username' => $status->user->username]) }}">
+                                    {{ $status->user->getNameOrUsername() }}</a></h4>
+                            <p>{{ $status->body }}</p>
+                            <ul class="list-inline">
+                                <li>{{ $status->created_at->diffForHumans() }}</li>
+                                <li><a href="">Like</a></li>
+                                <li>10 likes</li>
+                            </ul>
+                            {{--<div class="media">--}}
+                            {{--<a class="pull-left" href="#">--}}
+                            {{--<img class="media-object" alt="" src="">--}}
+                            {{--</a>--}}
+                            {{--<div class="media-body">--}}
+                            {{--<h4 class="media-heading"><a href="">Tyrion</a></h4>--}}
+                            {{--<p>Nothing to say</p>--}}
+                            {{--<ul class="list-inline">--}}
+                            {{--<li>5 minutes ago</li>--}}
+                            {{--<li><a href="">Like</a></li>--}}
+                            {{--<li>5 likes</li>--}}
+                            {{--</ul>--}}
+                            {{--</div>--}}
+                            {{--</div>--}}
+                            <form role="form" action="#" method="post">
+                                <div class="form-group">
+                                    <textarea name="reply-1" class="form-control" rows="4"
+                                              placeholder="Reply to this status"></textarea>
+                                </div>
+                                <input type="submit" value="Reply" class="btn btn-default btn-sm">
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
+                {!! $statuses->render() !!}
+            @endif
         </div>
     </div>
-
 @stop
