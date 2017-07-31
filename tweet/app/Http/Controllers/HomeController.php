b4919bb8f446ee806855d055f589549f0e51a746
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-31 18:41:26 +0200

    add function for textarea

--- tweet/app/Http/Controllers/HomeController.php
+++ tweet/app/Http/Controllers/HomeController.php
@@ -2,10 +2,16 @@
 
 namespace Tweet\Http\Controllers;
 
+use Auth;
+
 class HomeController extends Controller
 {
     public function index()
     {
+        if (Auth::check()) {
+            return view('timeline.index');
+        }
+
         return view('welcome');
     }
 }
