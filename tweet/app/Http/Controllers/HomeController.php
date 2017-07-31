eb4cb1e556411bbe12c288a3bf55ef54ca5aa583
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-31 19:51:38 +0200

    statuses query

--- tweet/app/Http/Controllers/HomeController.php
+++ tweet/app/Http/Controllers/HomeController.php
@@ -3,13 +3,22 @@
 namespace Tweet\Http\Controllers;
 
 use Auth;
+use Tweet\Models\Status;
 
 class HomeController extends Controller
 {
     public function index()
     {
         if (Auth::check()) {
-            return view('timeline.index');
+            $statuses = Status::where(function($query) {
+                return $query->where('user_id', Auth::user()->id)
+                    ->orWhereIn('user_id', Auth::user()->friends());
+            })
+                ->orderBy('created_at', 'desc')
+                ->paginate(10);
+
+            return view('timeline.index')
+                ->with('statuses', $statuses);
         }
 
         return view('welcome');
