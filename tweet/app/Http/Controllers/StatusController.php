4ae2761607c8abeca7fb198a14573e67a29d009c
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-08-02 11:18:34 +0200

    post reply function

--- tweet/app/Http/Controllers/StatusController.php
+++ tweet/app/Http/Controllers/StatusController.php
@@ -4,6 +4,7 @@ namespace Tweet\Http\Controllers;
 
 use Auth;
 use Illuminate\Http\Request;
+use Tweet\Models\Status;
 use Tweet\Models\User;
 
 class StatusController extends Controller
@@ -21,5 +22,32 @@ class StatusController extends Controller
         return redirect()
             ->route('welcome')
             ->with('info', 'Status posted already');
+    }
+
+    public function postReply(Request $request, $statusId)
+    {
+        $this->validate($request, [
+            "reply-{$statusId}" => 'required|max:800',
+        ], [
+            'required' => 'The reply is required.'
+        ]);
+
+        $status = Status::notReply()->find($statusId);
+
+        if (!$status) {
+            return redirect()->route('welcome');
+        }
+
+        if (!Auth::user()->isFriendsWith($status->user) && Auth::user()->id !== $status->user->id) {
+            return redirect()->route('welcome');
+        }
+
+        $reply = Status::create([
+            'body' => $request->input("reply-{$statusId}"),
+        ])->user()->associate(Auth::user());
+
+        $status->replies()->save($reply);
+
+        return redirect()->back();
     }
 }
