1b981a95d5f49d9ce6f198674b4f0e10d37551c0
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-08-07 10:49:38 +0200

    added counts of likes

--- tweet/resources/views/profile/index.blade.php
+++ tweet/resources/views/profile/index.blade.php
@@ -21,6 +21,11 @@
                             <p>{{ $status->body }}</p>
                             <ul class="list-inline">
                                 <li>{{ $status->created_at->diffForHumans() }}</li>
+                                @if($status->user->id !== Auth::user()->id)
+                                    <li><a href="{{ route('status.like', ['statusId' => $status->id]) }}">Like</a>
+                                    </li>
+                                @endif
+                                <li>{{ $status->likes->count() }} {{ str_plural('like', $status->likes->count()) }}</li>
                             </ul>
                             @foreach($status->replies as $reply)
                                 <div class="media">
@@ -37,9 +42,10 @@
                                         <ul class="list-inline">
                                             <li>{{ $reply->created_at->diffForHumans() }}</li>
                                             @if($reply->user->id !== Auth::user()->id)
-                                                <li><a href="{{ route('status.like', ['statusId' => $reply->id]) }}">Like</a></li>
-                                                <li>5 likes</li>
+                                                <li><a href="{{ route('status.like', ['statusId' => $reply->id]) }}">Like</a>
+                                                </li>
                                             @endif
+                                            <li>{{ $reply->likes->count() }} {{ str_plural('like', $reply->likes->count()) }}</li>
                                         </ul>
                                     </div>
                                 </div>
--- tweet/resources/views/timeline/index.blade.php
+++ tweet/resources/views/timeline/index.blade.php
@@ -38,8 +38,8 @@
                                 <li>{{ $status->created_at->diffForHumans() }}</li>
                                 @if($status->user->id !== Auth::user()->id)
                                     <li><a href="{{ route('status.like', ['statusId' => $status->id]) }}">Like</a></li>
-                                    <li>10 likes</li>
                                 @endif
+                                <li>{{ $status->likes->count() }} {{ str_plural('like', $status->likes->count()) }}</li>
                             </ul>
                             @foreach($status->replies as $reply)
                                 <div class="media">
@@ -57,8 +57,8 @@
                                             <li>{{ $reply->created_at->diffForHumans() }}</li>
                                             @if($reply->user->id !== Auth::user()->id)
                                                 <li><a href="{{ route('status.like', ['statusId' => $reply->id]) }}">Like</a></li>
-                                                <li>5 likes</li>
                                             @endif
+                                            <li>{{ $reply->likes->count() }} {{ str_plural('like', $reply->likes->count()) }}</li>
                                         </ul>
                                     </div>
                                 </div>
